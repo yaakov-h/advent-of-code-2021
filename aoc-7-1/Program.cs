ccf9c401d32cbd0df8822e6ad4b71f59add3f776
Yaakov <git@yaakov.dev>
2021-12-07 16:38:00 +1100

    day 7

---
+++ aoc-7-1/Program.cs
@@ -0,0 +1,27 @@
+﻿var input = File.ReadAllText("/Users/yaakov/Developer/Miniprojects/advent-of-code/aoc-7-1/input.txt");
+
+var values = Array.ConvertAll(input.Split(','), x => int.Parse(x));
+var maxValue = values.Max();
+var map = new int[maxValue + 1];
+foreach (var value in values)
+{
+    map[value] += 1;
+}
+
+var lowestCost = int.MaxValue;
+
+for (var i = 0; i <= maxValue; i++)
+{
+    var cost = 0;
+    for (var j = 0; j <= maxValue; j++)
+    {
+        cost += (Math.Abs(j - i) * map[j]);
+    }
+
+    if (cost < lowestCost)
+    {
+        lowestCost = cost;
+    }
+}
+
+Console.WriteLine(lowestCost);
---
+++ aoc-7-2/Program.cs
@@ -0,0 +1,39 @@
+﻿var input = File.ReadAllText("/Users/yaakov/Developer/Miniprojects/advent-of-code/aoc-7-2/input.txt");
+
+var values = Array.ConvertAll(input.Split(','), x => int.Parse(x));
+var maxValue = values.Max();
+var map = new int[maxValue + 1];
+foreach (var value in values)
+{
+    map[value] += 1;
+}
+
+var lowestCost = int.MaxValue;
+
+for (var i = 0; i <= maxValue; i++)
+{
+    var cost = 0;
+    for (var j = 0; j <= maxValue; j++)
+    {
+        cost += GetCost(i, j) * map[j];
+    }
+
+    if (cost < lowestCost)
+    {
+        lowestCost = cost;
+    }
+}
+
+Console.WriteLine(lowestCost);
+
+static int GetCost(int start, int end)
+{
+    var cost = 0;
+    var distance = Math.Max(start, end) - Math.Min(start, end);
+    for (var i = 0; i <= distance; i++)
+    {
+        cost += i;
+    }
+
+    return cost;
+}
