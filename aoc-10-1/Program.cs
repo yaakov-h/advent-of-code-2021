b19225718aaff7267e24aed757778f78b3dc00de
Yaakov <git@yaakov.dev>
2021-12-10 16:34:38 +1100

    Add day 10

---
+++ aoc-10-1/Program.cs
@@ -0,0 +1,56 @@
+﻿var input = File.ReadLines("/Users/yaakov/Developer/Miniprojects/advent-of-code/aoc-10-1/input.txt");
+
+var score = 0;
+foreach (var line in input)
+{
+    var result = GetFirstInvalidClosingChar(line);
+    if (result is null) continue;
+    var (expected, found) = result.Value;
+    Console.WriteLine($"Line '{line}' is corrupt. Expected '{expected}' but found '{found}'.");
+    score += GetScore(found);
+}
+Console.WriteLine(score);
+
+char GetClosingChar(char openingChar) => openingChar switch {
+    '(' => ')',
+    '[' => ']',
+    '{' => '}',
+    '<' => '>',
+    _ => throw new NotSupportedException(),
+};
+
+int GetScore(char illegalChar) => illegalChar switch {
+    ')' => 3,
+    ']' => 57,
+    '}' => 1197,
+    '>' => 25137,
+    _ => throw new NotSupportedException(),
+};
+
+(char expected, char found)? GetFirstInvalidClosingChar(string line)
+{
+    var state = new Stack<char>();
+
+    foreach (var c in line)
+    {
+        switch (c)
+        {
+            case '(' or '[' or '{' or '<':
+                state.Push(c);
+                break;
+
+            case ')' or ']' or '}' or '>':
+                var expectedClosingChar = GetClosingChar(state.Pop());
+                if (expectedClosingChar != c)
+                {
+                    return (expectedClosingChar, c);
+                }
+            break;
+
+            default:
+                throw new NotSupportedException();
+        }
+    }
+
+    return null;
+}
---
+++ aoc-10-2/Program.cs
@@ -0,0 +1,77 @@
+﻿var input = File.ReadLines("/Users/yaakov/Developer/Miniprojects/advent-of-code/aoc-10-2/input.txt");
+
+var scores = new List<long>();
+
+foreach (var line in input)
+{
+    var result = GetHypotheticalClosingChars(line);
+    if (result is null || result.Count == 0) continue;
+    var score = GetScore(result);
+    Console.WriteLine($"Line '{line}' is incomplete. To close, add: \"{string.Join(string.Empty, result)}\" with score {score}.");
+    scores.Add(score);
+}
+
+if ((scores.Count % 2) != 1)
+{
+    throw new Exception("Should have an odd amount of lines. :/");
+}
+
+scores.Sort();
+Console.WriteLine(string.Join(", ", scores));
+var middleIndex = ((scores.Count - 1) / 2);
+Console.WriteLine(scores[middleIndex]);
+
+char GetClosingChar(char openingChar) => openingChar switch {
+    '(' => ')',
+    '[' => ']',
+    '{' => '}',
+    '<' => '>',
+    _ => throw new NotSupportedException(),
+};
+
+long GetScore(List<char> chars)
+{
+    var score = 0L;
+
+    foreach (var c in chars)
+    {
+        score *= 5;
+        score += c switch {
+            ')' => 1,
+            ']' => 2,
+            '}' => 3,
+            '>' => 4,
+            _ => throw new ArgumentException(),
+        };
+    }
+
+    return score;
+};
+
+List<char>? GetHypotheticalClosingChars(string line)
+{
+    var state = new Stack<char>();
+
+    foreach (var c in line)
+    {
+        switch (c)
+        {
+            case '(' or '[' or '{' or '<':
+                state.Push(c);
+                break;
+
+            case ')' or ']' or '}' or '>':
+                var expectedClosingChar = GetClosingChar(state.Pop());
+                if (expectedClosingChar != c)
+                {
+                    return null;
+                }
+            break;
+
+            default:
+                throw new NotSupportedException();
+        }
+    }
+
+    return state.Select(x => GetClosingChar(x)).ToList();
+}
